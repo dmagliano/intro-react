d0b56d73691e9676131df382353e3fc816baf60e
dmagliano <diogo.magliano@gmail.com>
2020-07-07 00:45:24 -0300

    Initial Commit

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Repos from './github/ReposContainer'
+
+//stateless component - imprime HTML puro ou o que é passado pelas props
+
+const App = () => (
+  <Repos></Repos>
+)
+
+export default App
